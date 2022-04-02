bdc134172fe6047b0e3f0251c6f48a836636b509
dagrv <rust.91@icloud.com>
2022-04-02 22:48:49 +0200

    Product price with laravel-money

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Cknow\Money\Money;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function formattedPrice() {
+        return money($this->price);
+    }
 }
