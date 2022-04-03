1f86764d98154c78706229fcc24807e93b5e295c
dagrv <rust.91@icloud.com>
2022-04-03 18:06:24 +0200

    Add To Cart

--- resources/views/livewire/product-selector.blade.php
+++ resources/views/livewire/product-selector.blade.php
@@ -2,4 +2,12 @@
     @if ($initialVariation)
         <livewire:product-dropdown :variations="$initialVariation" />
     @endif
+
+    @if ($skuVariant)
+        <div class="mt-4">
+            <x-button wire:click.prevent="addToCart" class="font-semibold w-full justify-center text-md">
+                Add to Cart - {{ $skuVariant->formattedPrice() }}
+            </x-button>
+        </div>
+    @endif
 </div>
