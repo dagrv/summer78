18ba2c67eee591c69deec773861f4413348bcab3
dagrv <rust.91@icloud.com>
2022-04-05 01:50:03 +0200

    Cart

--- app/Models/Variation.php
+++ app/Models/Variation.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Image\Manipulations;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
 
 class Variation extends Model implements HasMedia
@@ -47,5 +49,15 @@ class Variation extends Model implements HasMedia
     public function product()
     {
         return $this->belongsTo(Product::class);
+    }
+
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this->addMediaConversion('thumb200x200')->fit(Manipulations::FIT_CROP, 200, 200);
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('default')->useFallbackUrl(url('/storage/no-image.png'));
     }
 }
