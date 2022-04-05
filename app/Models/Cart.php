9d1f4249377ece2160bc6be5fd63ec7cbf8dd8f5
dagrv <rust.91@icloud.com>
2022-04-05 03:54:04 +0200

    Cart Session for Guest Users

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Cart extends Model
 {
     use HasFactory;
+
+    public static function booted()
+    {
+        static::creating(function ($model) {
+            $model->uuid = (string) Str::uuid();
+        });
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
