36598c457620bef8fa7f5bb0ebf3a31f32b80c18
dagrv <rust.91@icloud.com>
2022-05-02 02:19:16 +0200

    Setting up orders

--- app/Http/Livewire/Checkout.php
+++ app/Http/Livewire/Checkout.php
@@ -3,15 +3,15 @@
 namespace App\Http\Livewire;
 
 use App\Cart\Contracts\CartInterface;
+use App\Models\Order;
 use App\Models\ShippingAddress;
 use App\Models\ShippingType;
 use Livewire\Component;
 
-class Checkout extends Component
-{
+class Checkout extends Component {
     public $shippingTypes;
     public $shippingTypeId;
-    public $shippingAddress;
+    protected $shippingAddress;
     public $userShippingAddressId;
     
     public $accountForm = ['email' => ''];
@@ -34,8 +34,7 @@ class Checkout extends Component
         'shippingForm.address.required' => 'Your :attribute is required',
     ];
 
-    public function rules()
-    {
+    public function rules() {
         return [
             'accountForm.email'      => 'required|email|max:255|unique:users,email' . (auth()->user() ? ',' . auth()->user()->id : ''),
             'shippingForm.address'   => 'required|max:255',
@@ -45,8 +44,7 @@ class Checkout extends Component
         ];
     }
 
-    public function updatedUserShippingAddressId($id)
-    {
+    public function updatedUserShippingAddressId($id) {
         if (!$id) {
             return;
         }
@@ -55,23 +53,35 @@ class Checkout extends Component
              ->only('address', 'city', 'postcode');
     }
 
-    public function getUserShippingAddressesProperty()
-    {
+    public function getUserShippingAddressesProperty() {
         return auth()->user()?->shippingAddresses;
     }
 
-    public function checkout()
-    {
+    public function checkout(CartInterface $cart) {
         $this->validate();
-        
-        ($this->shippingAddress = ShippingAddress::whereBelongsTo(auth()->user())->firstOrCreate($this->shippingForm))
+        $this->shippingAddress = ShippingAddress::query();
+
+        if (auth()->user()) {
+            $this->shippingAddress = $this->shippingAddress->whereBelongsTo(auth()->user()); 
+        }
+
+        ($this->shippingAddress = $this->shippingAddress->firstOrCreate($this->shippingForm))
             ?->user()
             ->associate(auth()->user())
             ->save();
+
+        $order = Order::make(array_merge($this->accountForm, [
+            'subtotal' => $cart->subtotal()
+        ]));
+
+        $order->user()->associate(auth()->user());
+        $order->shippingType()->associate($this->shippingType);
+        $order->shippingAddress()->associate($this->shippingAddress);
+
+        $order->save();
     }
 
-    public function mount()
-    {
+    public function mount() {
         $this->shippingTypes = ShippingType::orderBy('price', 'asc')->get();
         $this->shippingTypeId = $this->shippingTypes->first()->id;
 
@@ -80,18 +90,15 @@ class Checkout extends Component
         }
     }
 
-    public function getShippingTypeProperty()
-    {
+    public function getShippingTypeProperty() {
         return $this->shippingTypes->find($this->shippingTypeId);
     }
 
-    public function getTotalProperty(CartInterface $cart)
-    {
+    public function getTotalProperty(CartInterface $cart) {
         return $cart->subtotal() + $this->shippingType->price;
     }
 
-    public function render(CartInterface $cart)
-    {
+    public function render(CartInterface $cart) {
         return view('livewire.checkout', [
             'cart' => $cart,
         ]);
---
+++ app/Models/Order.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    public $fillable = [
+        'email',
+        'subtotal',
+        'placed_at',
+        'packaged_at',
+        'shipped_at'
+    ];
+
+    public $timestamps = [
+        'placed_at',
+        'packaged_at',
+        'shipped_at'
+    ];
+
+    public static function booted()
+    {
+        static::creating(function (Order $order) {
+            $order->placed_at = now();
+            $order->uuid = (string) Str::uuid();
+        });
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function shippingType()
+    {
+        return $this->belongsTo(ShippingType::class);
+    }
+
+    public function shippingAddress()
+    {
+        return $this->belongsTo(ShippingAddress::class);
+    }
+}
